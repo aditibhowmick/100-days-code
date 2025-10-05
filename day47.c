3a44cf0e43170f8f3ac189677583d3f5a3783148
Aditi Bhowmick <bhowmickaditi123@gmail.com>
2025-10-06 01:45:10 +0530

    Add files via upload

---
+++ day47.c
@@ -0,0 +1,37 @@
+Q93: Check if two strings are anagrams of each other.
+
+#include <stdio.h>
+#include <string.h>
+void sortString(char str[]) {
+    int i, j;
+    char temp;
+    int len = strlen(str);
+
+    for (i = 0; i < len - 1; i++) {
+        for (j = i + 1; j < len; j++) {
+            if (str[i] > str[j]) {
+                temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+int main() {
+    char str1[100], str2[100];
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+    if (strlen(str1) != strlen(str2)) {
+        printf("The strings are NOT anagrams.\n");
+        return 0;
+    }
+    sortString(str1);
+    sortString(str2);
+    if (strcmp(str1, str2) == 0)
+        printf("The strings are anagrams.\n");
+    else
+        printf("The strings are NOT anagrams.\n");
+    return 0;
+}
